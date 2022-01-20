fda39449216ad1712555d9fe9a5c7176e48f30f2
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2022-01-20 14:21:39 -0500

    setup, two letters

---
+++ client/src/Key.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Key = ({letter, color}) => {
+  return <div style={{backgroundColor: color}}>
+      {letter}
+  </div>;
+};
+
+export default Key;
---
+++ client/src/Keyboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Key from './Key'
+//rafce
+const Keyboard = () => {
+  
+  return <div>
+      <Key letter="A" color="grey" />
+      <Key letter="B" color="green" />
+  </div>;
+};
+
+export default Keyboard;
