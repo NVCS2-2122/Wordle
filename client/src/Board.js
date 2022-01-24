81beaef91c60f23c499f801fac8f1c1d67726ae4
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2022-01-24 13:17:18 -0500

    fake guess

--- client/src/Board.js
+++ client/src/Board.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Board = () => {
-  return <div></div>;
+function Cell({letter, color}) {
+    return <td style={{backgroundColor: color}}>
+        {letter}
+    </td>
+}
+
+function Row({guess, letters}) {
+    return <tr>
+        {guess.split('')
+            .map(l => <Cell letter={l} color={"blue"} />)
+        }
+    </tr>
+}
+
+const Board = ({letters}) => {
+  const [answer, setAnswer] = useState("REACT")
+  const [guesses, setGuesses] = useState(["ROBOT"])
+  return <div>
+      <Row guess={guesses[0]} letters={letters} />
+  </div>;
 };
 
 export default Board;
