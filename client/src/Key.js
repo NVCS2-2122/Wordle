b3cced915a95c6f2eda31ca9bfdbd80d0dcc8b83
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2022-01-25 13:38:55 -0500

    new letter updates

--- client/src/Key.js
+++ client/src/Key.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Key = ({letter, color}) => {
-  return <div style={{backgroundColor: color}}>
+const Key = ({letter, color, setNewLetter}) => {
+  const onKeyPress = (e) => {
+    setNewLetter(letter)
+  }
+  return <div 
+    onClick={onKeyPress}
+    style={{backgroundColor: color}}>
       {letter}
   </div>;
 };
